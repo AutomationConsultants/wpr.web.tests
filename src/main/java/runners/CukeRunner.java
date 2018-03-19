134068773eb17196a6ca746f4cbcbab37b04555a
ashkaps <kapoor.aishvarya001@gmail.com>
2018-03-20 00:57:55 +0530

    Now read cucumber options from cucumber.properties file. Refactored all files in "features" folder

--- src/main/java/runners/CukeRunner.java
+++ src/main/java/runners/CukeRunner.java
@@ -1,5 +1,11 @@
 package runners;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.commons.lang.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.AfterClass;
@@ -11,10 +17,13 @@ import cucumberReports.CucumberReporter;
 
 public class CukeRunner {
 	private static final Logger logger = LogManager.getLogger(CukeRunner.class);
+	private final String CUCUMBER_PROPERTY_FILE_PATH = "/src/main/resources/PropertyFiles/cucumber.properties"; 
+
 
 	@Test
 	public void test() {
 		logger.info("Starting test execution..");
+		setCucumberOptions();
 		JUnitCore jUnitCore = new JUnitCore();
 		Result result = jUnitCore.run(CucumberRunner.class);
 		logger.info("Cuke Runner Test" + result.toString());
@@ -24,5 +33,23 @@ public class CukeRunner {
 	@AfterClass
 	public static void after() {
 		new CucumberReporter().generateReport();
+	}
+	
+	private void setCucumberOptions() {
+		try {
+			String cukeOptions = "features";
+			FileInputStream file = new FileInputStream(new File(System.getProperty("user.dir")  + CUCUMBER_PROPERTY_FILE_PATH));
+			Properties cucumberProperties = new Properties();
+			cucumberProperties.load(file);
+			if(StringUtils.isNotEmpty(cucumberProperties.getProperty("features"))) {
+				cukeOptions = cukeOptions + "/" + cucumberProperties.getProperty("features");
+			}
+			if(StringUtils.isNotEmpty(cucumberProperties.getProperty("tags"))) {
+				cukeOptions = cukeOptions + " --tags " + cucumberProperties.getProperty("tags");
+			}
+			System.setProperty("cucumber.options", cukeOptions);
+		} catch (IOException e) {
+			logger.error(e);
+		}
 	}
 }
